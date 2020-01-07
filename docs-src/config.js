6c9403fa1c408c3c64c3d7f56fdcbbbb1ff38800
blryli <405549416@qq.com>
2020-01-07 14:51:30 +0000

    Deploying to gh-pages from master c99236b3948b60d8e45b561d80f9af8947d14d0d

--- docs-src/config.js
+++
@@ -1,3 +0,0 @@
-export const config = require('../package.json')
-
-export const upName = config.name.replace(/(^\w|-\w)/g, m => m.replace('-', '').toUpperCase())
---
+++ js/chunk-76e89f93.6c058afd.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-76e89f93"],{b93f:function(n,w,o){}}]);
+//# sourceMappingURL=chunk-76e89f93.6c058afd.js.map
